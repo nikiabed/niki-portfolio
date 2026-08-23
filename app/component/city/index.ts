87cc80b73050b028f0b0cd53465b244690d00148
nikiabed <nikiabedzade@gmail.com>
2026-08-23 23:24:21 +0330

    Fixed map

--- app/component/city/index.ts
+++ app/component/city/index.ts
@@ -1,2 +1,5 @@
-export * from './CityMap'
-export * from './MomentumlySection'
+export * from "./CityMap";
+export * from "./MomentumlySection";
+export * from "./ProjectsSection";
+export * from "./ThesisSection";
+export * from './CurrentlyExploringSection'
